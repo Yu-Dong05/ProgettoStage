cf9665af960f91de8da71d3821a42a9869f04a14
Yu-Dong05 <yu.dong0502.education@gmail.com>
2024-06-10 17:53:22 +0200

    Progresso Aggiornato "FileAlternatorConfigurator".

--- src/FileAlteratorConfigurator.java
+++ src/FileAlteratorConfigurator.java
@@ -2,6 +2,7 @@ import org.apache.commons.io.monitor.FileAlterationListener;
 import org.apache.commons.io.monitor.FileAlterationMonitor;
 import org.apache.commons.io.monitor.FileAlterationObserver;
 
+import java.io.File;
 import java.util.concurrent.Executor;
 
 public class FileAlteratorConfigurator {
@@ -14,20 +15,13 @@ public class FileAlteratorConfigurator {
     public FileAlteratorConfigurator(String path, int ms_delay) {
         this.path = path;
         this.delay = ms_delay;
-        monitor = new FileAlterationMonitor(ms_delay);
-        observer = new FileAlterationObserver(path);
+        monitor = new FileAlterationMonitor(this.delay);
+        observer = new FileAlterationObserver(new File(this.path));
         observer.addListener(listener);
         monitor.addObserver(observer);
     }
 
     public void startMonitoring() throws Exception {
-
-        Executor executor = new Executor() {
-            @Override
-            public void execute(Runnable command) {
-                command.run();
-            }
-        };
-        executor.execute(monitor);
+        this.monitor.start();
     }
 }
